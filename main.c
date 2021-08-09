b32c88726ef3c0d3f74e8d0c04f265db691ac81a
SmoothHacker <laglerscott@gmail.com>
2021-08-09 11:08:07 -0500

    Initial Boilerplate

--- main.c
+++ main.c
@@ -1,19 +1,25 @@
-#include <stdio.h>
-#include <linux/kvm.h>
-#include <sys/ioctl.h>
+#include <err.h>
 #include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <linux/kvm.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
 
-int main() {
+int main(void) {
+    int kvm, ret;
+
     printf("LateRegistration - Linux Kernel Harness\n");
 
-    int kvmFD = open("/dev/kvm", O_RDWR | O_CLOEXEC);
-    if (!kvmFD) {
-        fprintf(stderr, "[ERR] /dev/kvm does not exist on the host OS.\n");
-        exit(-1);
-    }
+    kvm = open("/dev/kvm", O_RDWR | O_CLOEXEC);
+    if (kvm == -1)
+        err(1, "/dev/kvm");
 
-    close(kvmFD);
-    return 0;
+    // Make sure we have the stable version of the API
+    ret = ioctl(kvm, KVM_GET_API_VERSION, NULL);
+    if (ret == -1)
+        err(1, "KVM_GET_API_VERSION");
+    if (ret != 12)
+        errx(1, "KVM_GET_API_VERSION %d, expected 12", ret);
 }
