b080bf08a461702fa94056a3645b8d5734331636
SmoothHacker <laglerscott@gmail.com>
2022-02-25 17:17:09 -0500

    fix: naming revision to os-handler functions

--- os-handler/main.c
+++ os-handler/main.c
@@ -20,19 +20,19 @@
 #include "fuzzRunner.h"
 
 #define DEV_MAJOR 24
-#define DEVICE_NAME "lr-hypervisor"
+#define DEVICE_NAME "tl-hypervisor"
 #define PORT_1 0xdead
 
 // Function Prototypes
-static int lr_driver_init(void);
-static void lr_driver_exit(void);
+static int tl_driver_init(void);
+static void tl_driver_exit(void);
 static int open(struct inode *inode, struct file *file);
 static int release(struct inode *inode, struct file *file);
 static ssize_t read(struct file *filp, char __user *buf, size_t len,
                     loff_t *off);
 static ssize_t write(struct file *filp, const char *buf, size_t len,
                      loff_t *off);
-static long lr_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
+static long tl_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
 
 // File operation sturcture
 static struct file_operations fops = {
@@ -40,7 +40,7 @@ static struct file_operations fops = {
     .read = read,
     .write = write,
     .open = open,
-    .unlocked_ioctl = lr_ioctl,
+    .unlocked_ioctl = tl_ioctl,
     .release = release,
 };
 
@@ -48,7 +48,7 @@ static dev_t device_major;
 
 // This function will be called when we open the Device file
 static int open(struct inode *inode, struct file *file) {
-  pr_alert("[!] lr-hypervisor opened\n");
+  pr_alert("[!] tl-hypervisor opened\n");
   return 0;
 }
 
@@ -69,8 +69,8 @@ static ssize_t write(struct file *filp, const char __user *buf, size_t len,
 }
 
 // This function will be called when we write IOCTL on the Device file
-static long lr_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
-  // pr_alert("[!] lr-hypervisor ioctl issued - cmd: %d\n", cmd);
+static long tl_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
+  // pr_alert("[!] tl-hypervisor ioctl issued - cmd: %d\n", cmd);
 
   switch (cmd) {
   case TAKE_SNAPSHOT:
@@ -89,7 +89,7 @@ static long lr_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
 }
 
 // Module Init function
-static int __init lr_driver_init(void) {
+static int __init tl_driver_init(void) {
   device_major = register_chrdev(32, DEVICE_NAME, &fops);
 
   if (device_major < 0) {
@@ -106,12 +106,12 @@ static int __init lr_driver_init(void) {
 }
 
 // Module exit function
-static void lr_driver_exit(void) {
+static void tl_driver_exit(void) {
   // Not needed since VM will be destroyed
 }
 
-module_init(lr_driver_init);
-module_exit(lr_driver_exit);
+module_init(tl_driver_init);
+module_exit(tl_driver_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Scott Lagler <laglerscott@gmail.com>");
